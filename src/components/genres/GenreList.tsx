0db12962348377156c3012a7d71be05e6b1327f3
Monecika <opreaconstatnin1990@gmail.com>
2025-10-24 13:25:45 +0200

    display genres

--- src/components/genres/GenreList.tsx
+++ src/components/genres/GenreList.tsx
@@ -1,12 +1,23 @@
 import useGenres from "../../hooks/useGenres";
+import getCroppedImageUrl from "../../services/image-url";
+import "./GenreList.css";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <ul className="list-unstyled">
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <li key={genre.id}>
+          <div className="d-flex gap-1 align-items-center mb-1 ps-3">
+            <img
+              src={getCroppedImageUrl(genre.image_background)}
+              alt=""
+              className="rounded-3 genre-img"
+            />
+            <p className="p-0 m-0 text-start genre-name">{genre.name}</p>
+          </div>
+        </li>
       ))}
     </ul>
   );
