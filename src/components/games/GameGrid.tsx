372ec0b5751163bf591dbb021db03755a476fea3
Monecika <opreaconstatnin1990@gmail.com>
2025-10-28 11:18:28 +0100

    implement infinite scrolling

--- src/components/games/GameGrid.tsx
+++ src/components/games/GameGrid.tsx
@@ -3,6 +3,7 @@ import type { GameQuery } from "../../App";
 import useGames from "../../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface GameGridProps {
   gameQuery: GameQuery;
@@ -17,37 +18,50 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
     isFetchingNextPage,
     fetchNextPage,
   } = useGames(gameQuery);
+
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const games = data?.pages.flatMap((p) => p.results) || [];
 
   if (error) return <p className="text-danger">{error.message}</p>;
 
+  if (isLoading)
+    return (
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+        {skeletons.map((s) => (
+          <div className="col" key={`initial-skeleton-${s}`}>
+            <GameCardSkeleton />
+          </div>
+        ))}
+      </div>
+    );
+
   return (
     <>
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
-        {isLoading
-          ? skeletons.map((s) => (
-              <div className="col" key={`initial-skeleton-${s}`}>
+      <InfiniteScroll
+        className="overflow-hidden"
+        dataLength={games.length}
+        next={() => fetchNextPage()}
+        hasMore={hasNextPage}
+        loader={
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 mt-0">
+            {skeletons.map((s) => (
+              <div className="col" key={`skeleton-${s}`}>
                 <GameCardSkeleton />
               </div>
-            ))
-          : data?.pages.map((page, pageIndex) => (
-              <React.Fragment key={pageIndex}>
-                {page.results.map((game) => (
-                  <div className="col" key={game.id}>
-                    <GameCard game={game} />
-                  </div>
-                ))}
-
-                {pageIndex === data.pages.length - 1 &&
-                  isFetchingNextPage &&
-                  skeletons.map((s) => (
-                    <div className="col" key={`skeleton-${s}`}>
-                      <GameCardSkeleton />
-                    </div>
-                  ))}
-              </React.Fragment>
             ))}
-      </div>
+          </div>
+        }
+        scrollThreshold={0.9}
+      >
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+          {games.map((game) => (
+            <div className="col" key={game.id}>
+              <GameCard game={game} />
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
 
       {hasNextPage && (
         <button
@@ -61,5 +75,4 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
     </>
   );
 };
-
 export default GameGrid;
